6539430daf685ff09ff5c545b1e27c1a199924bb
Cyprien <cypriengehu@example.com>
2026-01-06 15:06:48 +0100

    fix: eslint correction

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,33 +1,33 @@
-import { promisify } from "util";
-import redis from "redis";
+import { promisify } from 'util';
+import redis from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.asyncMethod = promisify(this.client.get).bind(this.client);
+  constructor() {
+    this.client = redis.createClient();
+    this.asyncMethod = promisify(this.client.get).bind(this.client);
 
-        this.client.on('error', (err) => {
-            console.log(`Redis client not connected to the server: ${err}`);
-        })
-    }
+    this.client.on('error', (err) => {
+      console.log(`Redis client not connected to the server: ${err}`);
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const value = await this.asyncMethod(key);
-        return value;
-    }
+  async get(key) {
+    const value = await this.asyncMethod(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        this.client.set(key, value);
-        this.client.expire(key, value);
-    }
+  async set(key, value, duration) {
+    this.client.set(key, value);
+    this.client.expire(key, duration);
+  }
 
-    async del(key) {
-        this.client.del(key);
-    }
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
