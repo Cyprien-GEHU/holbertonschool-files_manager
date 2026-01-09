1200b58969ccc792c92cd82ecc8e1a6790cb57b5
Cyprien <cypriengehu@example.com>
2026-01-09 16:39:18 +0100

    fix : task 3 forgot "" on hex

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -10,7 +10,7 @@ exports.postNew = async (req, res) => {
   const emailExist = await database.db.collection('users').findOne({ email });
   if (emailExist) return res.status(400).json({ error: 'Already exist' });
 
-  const hashMdp = crypto.createHash('sha1').update(password).digest(hex);
+  const hashMdp = crypto.createHash('sha1').update(password).digest('hex');
   const PostUser = await database.db.collection('users').insertOne({ email, password: hashMdp });
 
   return res.status(201).json({ id: PostUser.insertedId, email });
