b8b2448f0e447fec8fcda5d7c8c11920e8cd80da
Cyprien <cypriengehu@example.com>
2026-01-09 12:32:05 +0100

    fix: eslint correction

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,10 +1,10 @@
-import pkg from "mongodb";
+import pkg from 'mongodb';
 
 const { MongoClient } = pkg;
 
-const host = process.env.DB_HOST || "localhost";
-const port = process.env.DB_PORT || "27017";
-const database = process.env.DB_DATABASE || "files_manager";
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || '27017';
+const database = process.env.DB_DATABASE || 'files_manager';
 const uri = `mongodb://${host}:${port}`;
 
 class DBClient {
@@ -20,24 +20,24 @@ class DBClient {
       })
       .catch((err) => {
         this.connected = false;
-        console.log(err)
-      })
+        console.log(err);
+      });
   }
 
   isAlive() {
-    return this.connected
+    return this.connected;
   }
 
   async nbUsers() {
     if (!this.db) {
-      return 0
+      return 0;
     }
     return this.db.collection('users').countDocuments();
   }
 
   async nbFiles() {
     if (!this.db) {
-      return 0
+      return 0;
     }
     return this.db.collection('files').countDocuments();
   }
